00e8039d06db6a3c95e4c099a0c2aa4481643b5b
unknown <jihadmahmud71@gmail.com>
2023-01-18 01:29:39 +0600

    done project

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -40,6 +40,10 @@ export default function Login({setUser}) {
       
     } else {
       response = await registerUser({ email, password, username });
+      showAlert({
+        msg: `thanks for register please login`
+      });
+      navigate('/')
     }
     //navigate('/products');
 
